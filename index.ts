ab65a689908f19ca521f73950c2ae3b725353773
Philipp Hergenhahn <philipp.hergenhahn@setilic.com>
2022-12-15 11:13:19 +0100

    Adding joke

--- index.ts
+++ index.ts
@@ -1,6 +1,29 @@
 import { Deck } from './deck.ts';
 import { Player } from './player.ts';
 import { Logger } from './logger.ts';
+import axiod from 'https://deno.land/x/axiod/mod.ts';
+import process from 'https://deno.land/std@0.165.0/node/process.ts';
+
+//noinspection SpellCheckingInspection
+interface IJoke {
+   error: boolean;
+   category: string;
+   type: 'single' | 'twopart';
+   joke?: string;
+   setup?: string;
+   delivery?: string;
+   flags: {
+      nsfw: boolean;
+      religious: boolean;
+      political: boolean;
+      racist: boolean;
+      sexist: boolean;
+      explicit: boolean;
+   };
+   id: number;
+   safe: boolean;
+   lang: string;
+}
 
 function getPlayerAmount(): number {
    let playerAmount;
@@ -14,8 +37,33 @@ function getPlayerAmount(): number {
       }
       console.log('Please enter a number in the given range.');
    }
-   return playerAmount;
+   return playerAmount;1
 }
+process.stdout.write('Getting joke...');
+
+async function sleep(ms: number) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+await axiod
+   .get(
+      'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit',
+   )
+   .then(async x => {
+      console.log("Done");
+      const data = x.data as IJoke;
+      if (data.type === 'single') {
+         console.log("Today's joke:\n" + x.data.joke);
+      } else if (data.type === 'twopart') {
+         console.log(data.setup);
+         await sleep(2000);
+         console.log(data.delivery);
+      }
+      console.log('');
+   })
+   .catch((e: any) => {
+      console.log('Error getting joke: ' + e);
+   });
 
 const deck = new Deck();
 const dealer = new Player({ deck, dealer: true });
